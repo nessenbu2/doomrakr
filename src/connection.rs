74ee2099b2c17b22b5d98ed987cd1b30f1d5a057
Nick Essenburg <nessenbu@gmail.com>
2020-11-19 12:38:13 -0800

    Refactor connection code into something marginally more manageable

--- src/connection.rs
+++ src/connection.rs
@@ -4,22 +4,69 @@ use std::vec::Vec;
 use std::sync::{Arc, Mutex};
 use std::net::TcpStream;
 use std::io::Read;
+use std::ops::DerefMut;
 use std::thread;
 use headers::{Header, get_header_from_stream};
 
 enum State {
-    Connecting,
-    Connected,
+    Idle,
+    Streaming,
     Closed
 }
 
 pub struct Connection {
     client_id: String,
     socket :TcpStream,
-    state: State
+    state: State,
+    is_changed: bool
+}
+
+fn con_main(con_ref: Arc<Mutex<Connection>>) {
+    loop {
+        let mut con = con_ref.lock().unwrap();
+
+        if con.is_changed {
+            // TODO: clean-up
+        }
+
+        match con.state {
+            State::Idle => heartbeat(con.deref_mut()),
+            State::Streaming => send_chunk(con.deref_mut()),
+            State::Closed => {
+                clean_up(con.deref_mut());
+                break;
+            },
+        }
+    }
+}
+
+fn heartbeat(con: &mut Connection) {
+    let mut ack_header = Header{action: 1, length:0};
+    ack_header.send(&mut con.socket).unwrap();
+    let mut header = get_header_from_stream(&mut con.socket);
+    println!("Got data from client: {}, length: {}", con.client_id, header.length);
+    let mut data_buf = Vec::new();
+    data_buf.resize(header.length, 0);
+    con.socket.read(&mut data_buf);
+    drop(con);
+    let echo = String::from_utf8(data_buf).unwrap();
+    println!("{}", echo);
+}
+
+fn send_chunk(con: &mut Connection) {
+}
+
+fn clean_up(con: &mut Connection) {
+}
+
+fn start_heartbeating(con_mutex: Arc<Mutex<Connection>>) {
+    thread::spawn(move || {
+        con_main(Arc::clone(&con_mutex))
+    });
 }
 
 impl Connection {
+
     pub fn init_connection(mut socket: &mut TcpStream) -> Arc<Mutex<Connection>> {
         let header = get_header_from_stream(socket);
         if header.action != headers::CLIENT_ACK {
@@ -33,31 +80,13 @@ impl Connection {
         socket.read(&mut client_id_bytes);
         let mut ack_header = Header{action: 1, length:0};
         ack_header.send(&mut socket).unwrap(); // :J
-        let mut con = Connection{client_id: String::from_utf8(client_id_bytes).unwrap(),
+        let con_mutex = Arc::new(Mutex::new(Connection{
+                                client_id: String::from_utf8(client_id_bytes).unwrap(),
                                 socket: socket.try_clone().unwrap(),
-                                state: State::Connecting};
-        let con_mutex = Arc::new(Mutex::new(con));
-        Connection::start_heartbeating(con_mutex.clone());
+                                state: State::Idle,
+                                is_changed: false}));
+        start_heartbeating(con_mutex.clone());
         con_mutex
     }
 
-    pub fn start_heartbeating(con_mutex: Arc<Mutex<Connection>>) {
-        let con_ref = Arc::clone(&con_mutex);
-        // TODO: check connecting status
-        thread::spawn(move || {
-            let mut ack_header = Header{action: 1, length:0};
-            loop {
-                let mut con = con_ref.lock().unwrap();
-                ack_header.send(&mut con.socket).unwrap();
-                let mut header = get_header_from_stream(&mut con.socket);
-                println!("Got data from client: {}, length: {}", con.client_id, header.length);
-                let mut data_buf = Vec::new();
-                data_buf.resize(header.length, 0);
-                con.socket.read(&mut data_buf);
-                drop(con);
-                let echo = String::from_utf8(data_buf).unwrap();
-                println!("{}", echo);
-            }
-        });
-    }
 }
