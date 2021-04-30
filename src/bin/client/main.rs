d290b1290e8b9a361a3c97bb9a1332de263c0986
Nick Essenburg <nessenbu@gmail.com>
2021-04-29 19:45:36 -0700

    Accidentally changed the port somehow...

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -45,10 +45,10 @@ fn main() {
         None => return
     };
 
-    println!("Connection to {}...", hostname);
+    println!("Connecting to {}:6124...", hostname);
 
     loop {
-        match TcpStream::connect(format!("{}:6242", hostname)) {
+        match TcpStream::connect(format!("{}:6142", hostname)) {
             Ok(stream) => handle(stream, &mut client_id),
             Err(err) => {
                 // Should consider having a retryable class or errors but the only one I care about
